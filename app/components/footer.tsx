5a1ec0d45f128f97cfe565d7b841ee231de2cd4c
manahil <sufyanhingoro27@gmail.com>
2024-10-19 17:25:50 +0500

    first commit

---
+++ app/components/footer.tsx
@@ -0,0 +1,19 @@
+ export default function Footer(){
+     return(
+         <footer className="bg-gradient-to-r from-blue-900 to-purple-500 text-white p-10">
+             <div className="container mx-auto text-center">
+                 <h2 className="text-3xl font-bold mb-4"> Hello Everyone From Manahil</h2>
+                 <p className="text-lg mb-6">stay connected with us</p>
+                 <div className="flex justify-center space-x-4 mb-6">
+                     <button className="bg-pink-300 text-gray-800 font-semibold py-2 px-4 rounded hover:bg-pink-200">instagram</button>
+                     <button className="bg-yellow-300 text-gray-800 font-semibold py-2 px-4 rounded hover:bg-yellow-200">facebook</button>
+                     <button className="bg-green-300 text-gray-800 font-semibold py-2 px-4 rounded hover:bg-green-200">twitter</button>
+                     </div>
+                     <div className="text-sm">
+                         <p> 2024 Manahil. All rights reserved.</p>
+                   </div>
+                   </div>  
+                   </footer>
+
+    );
+}
